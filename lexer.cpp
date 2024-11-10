f4f004610310f3204886c5b7d9526ca2eafe6927
Sidak <magicalsidak@gmail.com>
2024-11-10 15:19:12 -0500

    onto something

--- lexer.cpp
+++ lexer.cpp
@@ -3,44 +3,102 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
+
 #define ll long long
 using namespace std;
 
-// define tokens
-// for
-// if
-// print
-// var
+// Token class
 class Token {
-    public:
-        string value;
-        int type;
+public:
+    string value;
+    int type;
 
-    Token(string a, int b): value(a), type(b) {};
+    Token(string a, int b) : value(a), type(b) {}
+
+    // Overloading the << operator to print Token objects
+    friend ostream& operator<<(ostream& os, const Token& token) {
+        os << "Value: " << token.value << " Type: " << token.type;
+        return os;
+    }
 };
 
 string fileToString(const string& filename) {
-    std::ifstream file(filename); // Open the file
+    ifstream file(filename); // Open the file
     if (!file) { // Check if the file opened successfully
-        std::cerr << "Could not open the file: " << filename << std::endl;
+        cerr << "Could not open the file: " << filename << endl;
         return "";
     }
 
-    std::ostringstream ss;
+    ostringstream ss;
     ss << file.rdbuf();
     return ss.str();
 }
 
-vector<Token> parseFile(string file) {
-    for (ll i = 0; i < file.length(); i++){
-
+template <typename T>
+void printVector(const vector<T>& vec) {
+    for (const T& element : vec) {
+        cout << element << " ";  // This will now use the overloaded << operator
     }
+    cout << endl;
 }
 
+unordered_map<string, int> tokenListGenrator() {
+    unordered_map<string, int> tokenListRef;
+    tokenListRef["var"] = 1;
+    tokenListRef["NewLine"] = 2;
+    tokenListRef["="] = 3;
+    tokenListRef["number"] = 4;
+    tokenListRef["varName"] = 5;
+    tokenListRef["print"] = 6;
+    return tokenListRef;
+}
 
+bool checkIfNum(string number) {
+    try {
+       stoi(number);
+    } catch (...){
+        return false;
+    }
+    return true;
+}
+
+vector<Token> parseFile(const string& file, const unordered_map<string, int>& tokenListRef) {
+    ll pointerOne = 0;
+    vector<Token> tokenList;
+
+    for (ll i = 0; i < file.length(); ++i) {
+        if (file[i] == ' ') {
+            string word = file.substr(pointerOne, i - pointerOne);
+            auto it = tokenListRef.find(word);
+            if (it != tokenListRef.end()) {
+                Token tokenGen(word, it->second);
+                tokenList.push_back(tokenGen);
+            } else if (checkIfNum(word)) {
+                Token tokenGen(word, 4);
+                tokenList.push_back(tokenGen);
+            } else if (word != ""){
+                Token tokenGen(word, 5);
+                tokenList.push_back(tokenGen);
+            }
+            pointerOne = i + 1;
+        }
+        else if (file[i] == '\n') {
+            pointerOne = i + 1;
+            Token tokenGen("NewLine", 2);
+            tokenList.push_back(tokenGen);
+        }
+    }
+    return tokenList;
+}
 
 int main() {
-    cout << fileToString("test.en");
-    string test = "var hello = 5";
+    string filename = "test.en";  // Ensure the file exists in the current directory
+    unordered_map<string, int> tokenListRef = tokenListGenrator();
+    vector<Token> tokenList = parseFile(fileToString(filename).append(" "), tokenListRef);
+
+    // Print the vector of tokens
+    printVector(tokenList);
+
     return 0;
 }
